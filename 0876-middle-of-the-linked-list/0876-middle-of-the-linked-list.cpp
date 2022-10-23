5f9ae0babe8ee076b9be115b1c379378554c70ef
Vipul kumar Patel <77206750+vipulkumar2002@users.noreply.github.com>
2022-10-23 14:46:20 +0530

    Time: 4 ms (47.85%), Space: 7 MB (71.36%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,27 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// Best solution slow and fast
 class Solution {
-private:
-    int getLength(ListNode* head){
-        int len = 0;
-        while(head !=NULL){
-            len++;
-            head= head->next;
-        }
-        return len;
-    }
+
 public:
     ListNode* middleNode(ListNode* head) {
-        int len = getLength(head);
-        int ans = (len/2);
         
-        ListNode* temp = head;
-        int cnt = 0;
-        while(cnt<ans){
-            temp = temp->next;
-            cnt++;
+        if(head == NULL || head->next == NULL){
+            return head;
         }
-        return temp;
+        
+        ListNode* slow = head ;
+        ListNode* fast = head->next;
+        
+        while(fast!=NULL){
+            fast = fast->next;
+            if(fast!=NULL){
+                fast = fast->next;
+            }
+            slow = slow->next;
+        }
+        return slow;
     }
 };
