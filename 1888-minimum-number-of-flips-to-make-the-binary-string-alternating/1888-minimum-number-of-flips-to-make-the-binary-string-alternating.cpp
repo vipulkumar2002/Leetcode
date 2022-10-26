1c5ff8db66954e09242c36a6358d5a1c98ae84bb
Vipul kumar Patel <77206750+vipulkumar2002@users.noreply.github.com>
2022-10-26 21:33:31 +0530

    Time: 70 ms (71.75%), Space: 11.6 MB (74.24%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   int minFlips(string s) {
+        int n=s.size();
+        vector<vector<int> > cnt(2, vector<int>(2, 0));
+        
+        // cnt[0][0] = number of '0' in even positions
+        // cnt[0][1] = number of '0' in odd  positions
+        // cnt[1][0] = number of '1' in even positions
+        // cnt[1][1] = number of '1' in odd  positions
+        
+        for(int i=0; i<n; i++){
+            cnt[s[i]-'0'][i%2]++;
+        }
+        int ans=cnt[1][0]+cnt[0][1]; // cost to make all '0' in even positions and all '1' in odd positions
+        ans=min(ans, cnt[0][0]+cnt[1][1]); // cost to make all '1' in even positions and all '0' in odd positions
+        for(int i=0; i<n; i++){
+            cnt[s[i]-'0'][i%2]--;
+            cnt[s[i]-'0'][(n+i)%2]++; // sending this character at the end of the string
+            ans=min(ans, cnt[1][0]+cnt[0][1]);
+            ans=min(ans, cnt[0][0]+cnt[1][1]);
+        }
+        return ans;
+    }
+};
